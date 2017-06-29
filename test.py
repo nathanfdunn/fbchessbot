d0e58573fa252f707edbec96f0459ff9c3c926e2
Nathan Dunn <nathanfdunn@gmail.com>
2017-06-28 21:14:51 -0400

    Added test skeletons

--- test.py
+++ test.py
@@ -19,7 +19,7 @@ def mock_send_message(recipient, text):
 
 sent_game_reps = []
 def mock_send_game_rep(recipient, game, perspective=True):
-	sent_messages.append((recipient, game.image_url(perspective)))
+	sent_game_reps.append((recipient, game.image_url(perspective)))
 
 sent_pgns = []
 def mock_send_pgn(recipient, game):
@@ -39,12 +39,30 @@ _sentinel = object()
 
 class CustomAssertions:
 	def assertLastMessageEquals(self, recipient, text, *, target_index=-1):
-		expected_response = recipient, text
+		expected_response = str(recipient), text
 		if not sent_messages:
 			raise AssertionError(f'No recorded messages. Cannot match {expected_response!r}')
 		last_message = sent_messages[target_index]
+		last_message = str(last_message[0]), last_message[1]
 		# This only works because we're multiply inheriting from unittest.TestCase as well
 		self.assertEqual(last_message, expected_response)
+		# Belt and suspenders...
+		self.assertNotEqual(expected_response[0], 'None')
+		self.assertNotEqual(last_message[0], 'None')
+
+	def assertLastGameRepEquals(self, recipient, rep_url, *, target_index=-1):
+		rep_url = 'https://fbchessbot.herokuapp.com/image/' + rep_url
+		expected_rep = str(recipient), rep_url
+		if not sent_game_reps:
+			raise AssertionError(f'No recorded game reps. Cannot match {expected_response!r}')
+		last_rep = sent_game_reps[target_index]
+		last_rep = str(last_rep[0]), last_rep[1]
+		# This only works because we're multiply inheriting from unittest.TestCase as well
+		self.assertEqual(last_rep, expected_rep)
+		# Belt and suspenders...
+		self.assertNotEqual(expected_rep[0], 'None')
+		self.assertNotEqual(last_rep[0], 'None')
+
 
 class BaseTest(unittest.TestCase, CustomAssertions):
 	expected_replies = None
@@ -66,15 +84,23 @@ class BaseTest(unittest.TestCase, CustomAssertions):
 		if expected_replies is _sentinel:
 			expected_replies = self.expected_replies
 
-		num_messages_start = len(sent_messages)
+		num_replies_start = len(sent_messages) + len(sent_game_reps) + len(sent_pgns)
 		fbchessbot.handle_message(recipient, message)
-		num_messages_finish = len(sent_messages)
-		actual_replies = num_messages_finish - num_messages_start
+		num_replies_finish = len(sent_messages) + len(sent_game_reps) + len(sent_pgns)
+		actual_replies = num_replies_finish - num_replies_start
 		
 		if expected_replies is not None and actual_replies != expected_replies:
-			# print(sent_messages[-actual_replies:])
-			raise AssertionError(f'Expected {expected_replies} new messages, got {actual_replies}')
+			raise AssertionError(f'Expected {expected_replies} replies, got {actual_replies}')
 
+	# For setting up the board
+	def perform_moves(self, white_id, black_id, move_list, clear=True):
+		while move_list:
+			move_pair = move_list.pop(0)
+			self.handle_message(white_id, move_pair[0])
+			if len(move_pair) > 1:
+				self.handle_message(black_id, move_pair[1])
+		if clear:
+			clear_mocks()
 
 class TestRegistration(BaseTest):
 	expected_replies = 1
@@ -103,7 +129,7 @@ class TestRegistration(BaseTest):
 
 	# Coming soon!
 	@unittest.expectedFailure
-	def test_name_cant_collide_when_registering(self):
+	def test_name_cannot_collide_when_registering(self):
 		self.handle_message(self.nate_id, 'My name is Nate')
 		with self.subTest():
 			self.handle_message(self.chad_id, 'My name is Nate')
@@ -140,7 +166,7 @@ class TestRegistration(BaseTest):
 
 	# Coming soon!
 	@unittest.expectedFailure
-	def test_name_cant_collide_when_renaming(self):
+	def test_name_cannot_collide_when_renaming(self):
 		self.handle_message(self.nate_id, 'My name is Nate', expected_replies=None)
 		self.handle_message(self.chad_id, 'My name is Chad', expected_replies=None)
 		self.handle_message(self.jess_id, 'My name is Jess', expected_replies=None)
@@ -212,9 +238,93 @@ class TestOpponentContext(BaseTest):
 		self.handle_message(self.nate_id, 'Play against Chad', expected_replies=1)
 		self.assertLastMessageEquals(self.nate_id, 'You are already playing against Chad')
 
-	def test_cant_set_opponent_context_on_nonplayer(self):
+	def test_cannot_set_opponent_context_on_nonplayer(self):
 		self.handle_message(self.nate_id, 'Play against Dave', expected_replies=1)
 		self.assertLastMessageEquals(self.nate_id, "No player named 'Dave'")
+
+class TestGameInitiation(BaseTest):
+	# expected_replies = 1
+
+	def setUp(self):
+		self.db.delete_all()
+		self.handle_message(self.nate_id, 'My name is Nate', expected_replies=None)
+		self.handle_message(self.chad_id, 'My name is Chad', expected_replies=None)
+		self.handle_message(self.jess_id, 'My name is Jess', expected_replies=None)
+		self.handle_message(self.izzy_id, 'My name is Izzy', expected_replies=None)
+
+		self.handle_message(self.nate_id, 'Play against Jess', expected_replies=None)
+		self.handle_message(self.jess_id, 'Play against Nate', expected_replies=None)
+		clear_mocks()
+
+	def test_cannot_start_new_game_without_context(self):
+		self.handle_message(self.chad_id, 'New game white')
+		self.assertLastMessageEquals(self.chad_id, "You aren't playing against anyone (Use command 'play against <name>')")
+
+	def test_can_start_new_game_as_white(self):
+		self.handle_message(self.nate_id, 'New game white', expected_replies=3)
+		self.assertLastGameRepEquals(self.nate_id, 'rnbqkbnr-pppppppp-8-8-8-8-PPPPPPPP-RNBQKBNR', target_index=-2)
+		self.assertLastMessageEquals(self.jess_id, 'Nate started a new game')
+		self.assertLastGameRepEquals(self.jess_id, 'RNBKQBNR-PPPPPPPP-8-8-8-8-pppppppp-rnbkqbnr', target_index=-1)
+
+	def test_can_start_new_game_as_black(self):
+		self.handle_message(self.nate_id, 'New game black', expected_replies=3)
+		# Note the order of the target_index's of these two. show_to_both sends to white first. Huh.
+		self.assertLastGameRepEquals(self.nate_id, 'RNBKQBNR-PPPPPPPP-8-8-8-8-pppppppp-rnbkqbnr', target_index=-1)
+		self.assertLastMessageEquals(self.jess_id, 'Nate started a new game')
+		self.assertLastGameRepEquals(self.jess_id, 'rnbqkbnr-pppppppp-8-8-8-8-PPPPPPPP-RNBQKBNR', target_index=-2)
+
+	@unittest.expectedFailure
+	def test_cannot_start_new_in_middle_of_game(self):
+		self.handle_message(self.nate_id, 'New game white', expected_replies=None)
+		clear_mocks()
+		self.handle_message(self.nate_id, 'New game white', expected_replies=1)
+		self.assertLastMessageEquals(self.nate_id, 'You already have an active game with Jess')
+
+class TestUndo: #(BaseTest):
+	def test_undo_offer(self):
+		pass
+	def test_undo_accepted(self):
+		pass
+	def test_undo_rejected(self):
+		pass
+
+class TestGamePlay: #(BaseTest):
+	def test_cannot_make_impossible_move(self):
+		pass
+	def test_cannot_move_on_opponent_turn(self):
+		pass
+	def test_cannot_make_ambiguous_move(self):
+		pass
+
+	def test_basic_moves(self):
+		pass
+
+	def test_check(self):
+		pass
+
+class TestGameFinish: #(BaseTest):
+	def test_checkmate(self):
+		pass
+
+	def test_resign(self):
+		pass
+
+	def test_draw(self):
+		pass
+
+class TestMiscellaneous: #(BaseTest):
+	def test_show(self):
+		pass
+
+	def test_help(self):
+		pass
+
+	def test_pgn(self):
+		pass
+
+	def test_pgns(self):
+		pass
+	# def pass
 
 	# 	pass
 
